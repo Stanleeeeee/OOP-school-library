637099ac0ed99358150927e15a7a32b5788c05fd
Ezeoke Stanley <ezeokestanley.ifeanyi@gmail.com>
2022-08-08 13:56:38 +0100

    Add classes for person

---
+++ person.rb
@@ -0,0 +1,11 @@
+class Person
+  def initalize (name = 'Unknown', age, parent_permission: true)
+    @id = Random.rand(1...1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  attr_reader :id
+
+  attr_accessor :name, :age

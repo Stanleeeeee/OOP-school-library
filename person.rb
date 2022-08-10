211f7db9ec910adbbe29de8d5fb7cf10f1bf0d41
Ezeoke Stanley <ezeokestanley.ifeanyi@gmail.com>
2022-08-10 11:14:47 +0100

    Effect corrections from TSE

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require './nameable'
 
-class Person
-  def initalize(age, name = 'Unknown', parent_permission: true)
+class Person < Nameable
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1...1000)
     @name = name
